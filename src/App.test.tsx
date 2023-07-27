cbdeb7004d16f24388b0c82667bb6ce8929c5939
juan moreno <jcmm.info@gmail.com>
2023-07-27 10:35:52 -0500

    remove test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders headline', () => {
-  render(<App />)
-  const headline = screen.getByText(/TODO/)
-  expect(headline).toBeInTheDocument()
-})
--- src/components/__test__/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Header } from '../Header'
-
-test('renders headline', () => {
-  render(<Header title='Well to be honest' addNewTodo={() => {}} />)
-  const headingElement = screen.getByRole('heading', { name: 'Well to be honest' })
-  expect(headingElement).toBeInTheDocument()
-})
